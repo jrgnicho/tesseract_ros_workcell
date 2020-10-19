364270870964a28bab8aff14a1c7efdd2227024b
Jorge Nicho <jrgnichodevel@gmail.com>
2020-10-19 17:02:36 -0500

    loads seed to speed planning, currently an assertion failure terminates the application

--- twc_application/src/application_node.cpp
+++ twc_application/src/application_node.cpp
@@ -1,6 +1,8 @@
 #include <ros/ros.h>
 #include <ros/package.h>
+#include <boost/filesystem.hpp>
 #include <geometry_msgs/PoseArray.h>
+#include <visualization_msgs/MarkerArray.h>
 #include <tesseract_msgs/GetMotionPlanAction.h>
 #include <tesseract_rosutils/plotting.h>
 #include <actionlib/client/simple_action_client.h>
@@ -21,10 +23,117 @@
 using namespace tesseract_planning;
 
 static const std::string TOOLPATH = "twc_toolpath";
+static const std::string OUTPUT_PLAN_FILE_PATH = "output_plan.xml";
 static const std::vector<std::string> HOME_JOINTS = { "positioner_joint_1", "positioner_joint_2", "robot_joint_1",
                                                       "robot_joint_2",      "robot_joint_3",      "robot_joint_4",
                                                       "robot_joint_5",      "robot_joint_6" };
 static const std::vector<double> HOME_POSITION = { 0, 0, 0, -0.349066, 0.349066, 0, 0, 0 };
+
+static geometry_msgs::Pose pose3DtoPoseMsg(const std::array<float, 6>& p)
+{
+  using namespace Eigen;
+  geometry_msgs::Pose pose_msg;
+  Eigen::Affine3d eigen_pose = Translation3d(Vector3d(std::get<0>(p), std::get<1>(p), std::get<2>(p))) *
+                               AngleAxisd(std::get<3>(p), Vector3d::UnitX()) *
+                               AngleAxisd(std::get<4>(p), Vector3d::UnitY()) *
+                               AngleAxisd(std::get<5>(p), Vector3d::UnitZ());
+
+  pose_msg = tf2::toMsg(eigen_pose);
+  return std::move(pose_msg);
+}
+
+
+visualization_msgs::MarkerArray convertToAxisMarkers(const std::vector<geometry_msgs::PoseArray>& path,
+                                          const std::string& frame_id,
+                                          const std::string& ns,
+                                          const std::size_t& start_id = 1,
+                                          const double& axis_scale =  0.001,
+                                          const double& axis_length = 0.01,
+                                          const std::array<float, 6>& offset =  { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 })
+{
+  using namespace Eigen;
+
+  visualization_msgs::MarkerArray markers;
+
+  auto create_line_marker = [&](const int id,
+                                const std::tuple<float, float, float, float>& rgba) -> visualization_msgs::Marker {
+    visualization_msgs::Marker line_marker;
+    line_marker.action = line_marker.ADD;
+    std::tie(line_marker.color.r, line_marker.color.g, line_marker.color.b, line_marker.color.a) = rgba;
+    line_marker.header.frame_id = frame_id;
+    line_marker.type = line_marker.LINE_LIST;
+    line_marker.id = id;
+    line_marker.lifetime = ros::Duration(0);
+    line_marker.ns = ns;
+    std::tie(line_marker.scale.x, line_marker.scale.y, line_marker.scale.z) = std::make_tuple(axis_scale, 0.0, 0.0);
+    line_marker.pose = pose3DtoPoseMsg(offset);
+    return std::move(line_marker);
+  };
+
+  // markers for each axis line
+  int marker_id = start_id;
+  visualization_msgs::Marker x_axis_marker = create_line_marker(++marker_id, std::make_tuple(1.0, 0.0, 0.0, 1.0));
+  visualization_msgs::Marker y_axis_marker = create_line_marker(++marker_id, std::make_tuple(0.0, 1.0, 0.0, 1.0));
+  visualization_msgs::Marker z_axis_marker = create_line_marker(++marker_id, std::make_tuple(0.0, 0.0, 1.0, 1.0));
+
+  auto add_axis_line = [](const Isometry3d& eigen_pose,
+                          const Vector3d& dir,
+                          const geometry_msgs::Point& p1,
+                          visualization_msgs::Marker& marker) {
+    geometry_msgs::Point p2;
+    Eigen::Vector3d line_endpoint;
+
+    // axis endpoint
+    line_endpoint = eigen_pose * dir;
+    std::tie(p2.x, p2.y, p2.z) = std::make_tuple(line_endpoint.x(), line_endpoint.y(), line_endpoint.z());
+
+    // adding line
+    marker.points.push_back(p1);
+    marker.points.push_back(p2);
+  };
+
+  for (auto& poses : path)
+  {
+    for (auto& pose : poses.poses)
+    {
+      Eigen::Isometry3d eigen_pose;
+      tf2::fromMsg(pose, eigen_pose);
+
+      geometry_msgs::Point p1;
+      std::tie(p1.x, p1.y, p1.z) = std::make_tuple(pose.position.x, pose.position.y, pose.position.z);
+
+      add_axis_line(eigen_pose, Vector3d::UnitX() * axis_length, p1, x_axis_marker);
+      add_axis_line(eigen_pose, Vector3d::UnitY() * axis_length, p1, y_axis_marker);
+      add_axis_line(eigen_pose, Vector3d::UnitZ() * axis_length, p1, z_axis_marker);
+    }
+  }
+
+  markers.markers.push_back(x_axis_marker);
+  markers.markers.push_back(y_axis_marker);
+  markers.markers.push_back(z_axis_marker);
+  return std::move(markers);
+}
+
+visualization_msgs::MarkerArray convertToAxisMarkers(const std::vector<std::vector<Eigen::Isometry3d>>& path,
+                                                     const std::string& frame_id,
+                                                     const std::string& ns,
+                                                     const std::size_t& start_id = 1,
+                                                     const double& axis_scale =  0.001,
+                                                     const double& axis_length = 0.01,
+                                                     const std::array<float, 6>& offset =  { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 })
+{
+  std::vector<geometry_msgs::PoseArray> path_vec_msg;
+  for(const auto&v : path)
+  {
+    geometry_msgs::PoseArray path_msg;
+    for(const Eigen::Isometry3d& p : v)
+    {
+      path_msg.poses.push_back(tf2::toMsg(p));
+    }
+    path_vec_msg.push_back(path_msg);
+  }
+  return convertToAxisMarkers(path_vec_msg, frame_id, ns, start_id, axis_scale, axis_length, offset);
+}
 
 ///
 /// \brief parsePathFromFile Creates a collection of raster strips from a yaml file
@@ -226,17 +335,24 @@ CompositeInstruction createCartesianProgram(Eigen::Isometry3d tcp)
 
 int main(int argc, char** argv)
 {
+  namespace fs = boost::filesystem;
+
   ros::init(argc, argv, "application_node");
   ros::NodeHandle nh, pnh("~");
 
   std::string tool_path = ros::package::getPath("twc_application") + "/config/job_path.yaml";
-  pnh.param<std::string>("tool_path", tool_path);
+  std::string seed_plan = "";
+  tool_path = pnh.param<std::string>("tool_path", tool_path);
+  seed_plan = pnh.param<std::string>("seed_plan", seed_plan);
   ROS_INFO("Using tool path file: %s", tool_path.c_str());
 
   // Create a tesseract interface
   tesseract_monitoring::TesseractMonitorInterface interface("tesseract_workcell");
   tesseract::Tesseract::Ptr thor = interface.getTesseract<tesseract_environment::OFKTStateSolver>("tesseract_workcell_environment");
   auto current_transforms = thor->getEnvironment()->getCurrentState()->link_transforms;
+
+  // creating marker array publisher for toolpaths
+  ros::Publisher toolpath_pub = nh.advertise<visualization_msgs::MarkerArray>("input_toolpath", 1);
 
   // Dynamically load ignition visualizer if exist
   tesseract_visualization::VisualizationLoader loader;
@@ -274,8 +390,33 @@ int main(int argc, char** argv)
   std::vector<std::vector<Eigen::Isometry3d>> paths;
   parsePathFromFile(paths, tool_path);
   std::vector<std::vector<Eigen::Isometry3d>> filtered_path = filterPath(paths);
+
+  // publishing path
+  visualization_msgs::MarkerArray toolpath_markers = convertToAxisMarkers(filtered_path, "part_link", "twc");
+  toolpath_pub.publish(toolpath_markers);
+
+  // create program
   CompositeInstruction program = createProgram(filtered_path, "", tcp * rot_offset, transform);
   goal.request.name = goal.RASTER_G_FT_PLANNER_NAME;
+
+  // loading seed path
+  if(!seed_plan.empty())
+  {
+    if(!fs::exists(fs::path(seed_plan)))
+    {
+      ROS_ERROR("Seed path file %s does not exists", seed_plan.c_str());
+      return -1;
+    }
+    std::ifstream seed_plan_stream(seed_plan);
+    std::stringstream ss;
+    ss << seed_plan_stream.rdbuf();
+    goal.request.seed = ss.str();
+    ROS_INFO("Loaded seed plan file %s",seed_plan.c_str());
+  }
+  else
+  {
+    ROS_WARN("Seed plan file not loaded, skipping");
+  }
 
 //  Eigen::VectorXd jp = Eigen::VectorXd::Zero(6);
 //  jp(0) = M_PI_2;
@@ -295,14 +436,22 @@ int main(int argc, char** argv)
   goal.request.instructions = toXMLString(program);
   goal.request.num_threads = 1;
 
+  ros::Time start_time = ros::Time::now();
   ac.sendGoal(goal);
   ac.waitForResult();
 
   actionlib::SimpleClientGoalState state = ac.getState();
   ROS_INFO("Action finished: %s", state.toString().c_str());
+  ROS_WARN("Planning time: %f", (ros::Time::now() - start_time).toSec());
 
   auto result = ac.getResult();
   Instruction program_results = fromXMLString(result->response.results);
+
+  // saving program into file
+  std::ofstream output_plan_stream(OUTPUT_PLAN_FILE_PATH);
+  output_plan_stream << result->response.results;
+  output_plan_stream.close();
+  ROS_INFO("Saved output plan file to location %s", OUTPUT_PLAN_FILE_PATH.c_str());
 
   if (!result->response.successful)
   {
